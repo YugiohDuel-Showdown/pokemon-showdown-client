47517a6895664841d86d0686c10928bdae7b57d2
Cody Spratford <koreanpanda345@gmail.com>
2025-04-17 12:48:26 -0700

    Update battle-animations.ts

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -1646,6 +1646,9 @@ export class BattleScene implements BattleSceneStub {
 			case 5:
 				this.bgm = BattleSound.loadBgm(`dl-yugo.mp3`, 302000, 1812000, this.bgm);
 				break;
+			case 6:
+				this.bgm = BattleSound.loadBgm(`dl-leo.mp3`, 186000, 1116000, this.bgm);
+				break;
 			default:
 				this.bgm = BattleSound.loadBgm(`tdotr-1.mp3`, 1029000, 2058000, this.bgm);
 				break;
