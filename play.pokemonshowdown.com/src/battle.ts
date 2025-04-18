c7aa05ed012d5c6ded147e1d20b40c5fc40f9096
Cody Spratford <koreanpanda345@gmail.com>
2025-04-18 10:11:47 -0700

    Update battle.ts

--- play.pokemonshowdown.com/src/battle.ts
+++ play.pokemonshowdown.com/src/battle.ts
@@ -3088,10 +3088,11 @@ export class Battle {
 			this.activateAbility(poke, fromeffect);
 			let minTimeLeft = 5;
 			let maxTimeLeft = 0;
-			if (effect.id.endsWith('terrain')) {
+			const terrainList = ['mountain', 'wasteland', 'yami', 'zombieworld', 'harpieshuntingground'];
+			if (effect.id.endsWith('terrain') || terrainList.includes(effect.id)) {
 				for (let i = this.pseudoWeather.length - 1; i >= 0; i--) {
 					let pwID = toID(this.pseudoWeather[i][0]);
-					if (pwID.endsWith('terrain')) {
+					if (pwID.endsWith('terrain') || terrainList.includes(pwID)) {
 						this.pseudoWeather.splice(i, 1);
 						continue;
 					}
