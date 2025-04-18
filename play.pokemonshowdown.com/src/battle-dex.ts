f4c924f1c6589659fa438831f6665e2e173fee2e
Cody Spratford <koreanpanda345@gmail.com>
2025-04-18 14:47:24 -0700

    Update battle-dex.ts

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -888,7 +888,7 @@ export const Dex = new class implements ModdedDex {
 		const data = this.getTeambuilderSpriteData(pokemon, gen);
 		const shiny = (data.shiny ? '-shiny' : '');
 		data.x = 20;
-		data.y = 10;
+		data.y = 30;
 		let url = `https://raw.githubusercontent.com/YugiohDuel-Showdown/Yugioh-Monster-Sprites/refs/heads/main/front${shiny ? '-shiny' : ''}/${toID(pokemon.species)}.png`;
 		return `background-image:url(${url});background-position:${data.x}px ${data.y}px;background-repeat:no-repeat`;
 	}
