efa39a83314fd8219314b8b029a5a0f3d6b66c37
Cody Spratford <koreanpanda345@gmail.com>
2025-04-19 20:59:35 -0700

    Update battle-animations.ts

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -864,6 +864,8 @@ export class BattleScene implements BattleSceneStub {
 				if (textBuf) textBuf += ' / ';
 				textBuf += pokemon.speciesForme;
 				let url = spriteData.url;
+				spriteData.w = 64;
+				spriteData.h = 64;
 				// if (this.paused) url.replace('/xyani', '/xy').replace('.gif', '.png');
 				buf += `<img src="${url}" width="${spriteData.w}" height="${spriteData.h}" style="position:absolute;top:${Math.floor(y - spriteData.h / 2)}px;left:${Math.floor(x - spriteData.w / 2)}px" />`;
 				buf2 += `<div style="position:absolute;top:${y + 45}px;left:${x - 40}px;width:80px;font-size:10px;text-align:center;color:#FFF;">`;
